3086024faa0b2e3458da19217e7ed32f617018ab
MINHTAM\MinhTam <phanminhtam247@gmail.com>
2017-08-24 09:48:36 +0700

    Connect to Postgre SQL

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,9 +1,15 @@
+var tmp = require('../models/category');
+var getcategory;
+tmp.GetCategories(function( err, category){
+    getcategory = category;
+});
 var homeController = {
     index: function(req, res){
         res.render('index',{
             title: 'index page',
             message: 'Hello page',
-            page: 'index'
+            page: 'index',
+            category: getcategory
         })
     }
 }
---
+++ app/models/category.js
@@ -0,0 +1,18 @@
+var pg = require('pg');
+var ConectionString = 'postgres://postgres:@localhost:5432/QuanlyDD';
+var category = {
+    GetCategories: function( callback ){
+        var client = new pg.Client(ConectionString);
+        client.connect(function(err){
+            if (err != null) throw err;
+            client.query('Select id, name from categories', function( err, result){
+                if (err)
+                    console.log(err);
+                else
+                    console.log(JSON.stringify(result));
+                callback(err, result.rows);
+            });
+        });
+    }
+}
+module.exports = category;
